7cf95b9b7da9783b14131a33b5cf89ed16f07213
Allan Jardine <allan@datatables.net>
2022-10-10 19:50:52 +0000

    Add support for mjs files (ES Modules)

---
+++ js/scroller.foundation.min.mjs
@@ -0,0 +1,1 @@
+import $ from"jquery";import DataTable from"datatables.net-zf";import DataTable from"datatables.net-scroller";export default DataTable;
---
+++ js/scroller.foundation.mjs
@@ -0,0 +1,13 @@
+
+/*! Foundation styling wrapper for Scroller
+ * © SpryMedia Ltd - datatables.net/license
+ */
+
+import $ from 'jquery';
+import DataTable from 'datatables.net-zf';
+import DataTable from 'datatables.net-scroller';
+
+
+
+
+export default DataTable;
